dcede0c05b4611778b9d5362b0a4252ffe537f8f
kac5677-sketch <kac5677@gmail.com>
2025-08-17 16:11:26 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import "./index.css";   // ✅ 여기에 추가!
 
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
